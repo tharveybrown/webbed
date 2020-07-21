eb4cc67013177d952e762fd4ed3afebcd88939b8
tharveybrown <tharveybrown@gmail.com>
2020-07-21 11:59:40 -0700

    slack works

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,57 +2,97 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import { Paper, Button } from "@material-ui/core";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
+import { withStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+const url = runtimeEnv().REACT_APP_API_URL;
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+});
 
 // const dotenv = require("dotenv");
 
 /*It is created as a component function in the react hooks.*/
-function Dashboard({ handleLogout, isLoggedIn }) {
-  const token = localStorage.getItem("token");
-  const handleSlackAuth = () => {
-    if (token) {
-      fetch(
-        `https://slack.com/oauth/v2/authorize?client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}&scope=channels:history,channels:read,groups:read,users.profile:read,users:read,users:read.email,chat:write&user_scope=channels:history,channels:read,groups:history,im:history`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      ).then(console.log);
-    }
-  };
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slackId: "",
+      slackName: "",
+      loading: false,
+    };
+  }
 
-  return (
-    <div>
-      <h2>Dashboard Component</h2>
-      {isLoggedIn ? (
-        <Paper>
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            color="secondary"
-            component={Link}
-            to="/login"
-            onClick={handleLogout}
-          >
-            Logout
-          </Button>
-          <Button onClick={handleSlackAuth}>Add To Slack</Button>
-          <a
-            href={`https://slack.com/oauth/v2/authorize?client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}&scope=channels:history,channels:read,groups:read,users.profile:read,users:read,users:read.email,chat:write&user_scope=channels:history,channels:read,groups:history,im:history&token=${token}`}
-          >
-            <img
-              alt="Add to Slack"
-              height="40"
-              width="139"
-              src="https://platform.slack-edge.com/img/add_to_slack.png"
-              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-            />
-          </a>
-        </Paper>
-      ) : null}
-    </div>
-  );
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+
+    if (this.props.location.search) {
+      this.setState({ loading: true });
+      fetch(`${url}/auth/callback/${this.props.location.search}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          localStorage.setItem("slack_token", res.slack_token);
+          this.setState({
+            slackId: res.slack.slack_id,
+            slackName: res.slack.name,
+            loading: false,
+          });
+          this.props.history.push("/dashboard");
+        });
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        {this.state.slackName ? <h2>{this.state.slackName}</h2> : null}
+        {this.props.isLoggedIn ? (
+          <Paper>
+            <Button
+              type="submit"
+              fullWidth
+              variant="outlined"
+              color="secondary"
+              component={Link}
+              to="/login"
+              onClick={this.props.handleLogout}
+            >
+              Logout
+            </Button>
+            <div className={classes.wrapper}>
+              <a
+                href={`https://slack.com/oauth/v2/authorize?client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}&scope=channels:history,channels:read,groups:read,users.profile:read,users:read,users:read.email,chat:write&user_scope=channels:history,channels:read,groups:history,im:history&redirect_uri=http://localhost:3000/dashboard`}
+              >
+                <img
+                  alt="Add to Slack"
+                  height="40"
+                  width="139"
+                  src="https://platform.slack-edge.com/img/add_to_slack.png"
+                  srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+                />
+                {this.state.loading && (
+                  <CircularProgress size={38} className={classes.fabProgress} />
+                )}
+              </a>
+            </div>
+          </Paper>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default Dashboard;
+export default withStyles(styles)(Dashboard);
--- src/components/Register/Step3.js
+++ src/components/Register/Step3.js
@@ -56,6 +56,17 @@ function Step3(props) {
           </li>
         );
       })}
+      <a
+        href={`https://slack.com/oauth/v2/authorize?client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}&scope=channels:history,channels:read,groups:read,users.profile:read,users:read,users:read.email,chat:write&user_scope=channels:history,channels:read,groups:history,im:history`}
+      >
+        <img
+          alt="Add to Slack"
+          height="40"
+          width="139"
+          src="https://platform.slack-edge.com/img/add_to_slack.png"
+          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+        />
+      </a>
 
       {/* </Paper> */}
     </div>
